3149ebcd0208ab524f794da9b277c6d4b8280052
miji5757 <154939865+miji5757@users.noreply.github.com>
2024-06-13 20:37:38 +0900

    Add files via upload

---
+++ MediConnect/src/main/java/com/KJNP/MediConnect/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.KJNP.MediConnect;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MediConnectApplication.class);
+	}
+
+}
